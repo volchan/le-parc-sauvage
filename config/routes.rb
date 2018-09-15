6acfd080fb83b3efc73066de70ee5970ebb66ad7
Claire Gautier <claire.gautier@edhec.com>
2018-09-15 14:25:49 +0200

    save work to merge master

--- config/routes.rb
+++ config/routes.rb
@@ -26,4 +26,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_15_103859) do
+ActiveRecord::Schema.define(version: 2018_09_15_113136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_09_15_103859) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
