14ccc898e1533af6bbb8aaab7a086a2b763aeab8
Sybille Cordeau <sybillecordeau@gmail.com>
2018-09-15 14:26:40 +0200

    Add files scss team + avatar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home, :contact, :team, :sponsorship
+  skip_before_action :authenticate_user!, only: %I[home contact team sponsorship]
 
   def home; end
 
