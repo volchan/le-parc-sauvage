cffa7b569c13bd0028ec687c8a200a0ec350025e
stephane de la torre <s.dlt@hotmail.fr>
2018-09-16 15:41:35 +0200

    Fix pdf on heroku

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,1 +1,4 @@
-  WickedPdf.config = {}
+if Rails.env.production?
+  wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-amd64"
+  WickedPdf.config = { exe_path: wkhtmltopdf_path, wkhtmltopdf: wkhtmltopdf_path }
+end
